3769ba403662cf390c542d5a0d3f1248fc578eae
keigo takamura <s15528@salsio-sp.ac.jp>
2019-02-15 04:11:29 +0900

    start

---
+++ nupzak.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+#define objc_max 20  //オブジェクトの個数
+#define ga_mon 9
+#define  zac_max 200  //ザックの最大容量
+class nupzak
+{
+private:
+    
+public:
+    bool  zac_gac[ga_mon][objc_max];
+    int   objcs[objc_max];
+    
+
+};
+
+
+
+
+
+int main(){
+    
+
+
+    
+    
+    return 0;
+}
+    
